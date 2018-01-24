5cc91a04236b65999ad83d02f400c1243702a2a1
Wei HongJin <weihj1999@gmail.com>
2018-01-24 15:24:23 +0100

    udpate var

--- dns/bas02/var-changeme.tf
+++ dns/bas02/var-changeme.tf
@@ -1,35 +1,39 @@
 variable "username" {
   # If you don't fill this in, you will be prompted for it
-  default = "weihongjin"
-  #default = "temp-weihongjin"
+  default = "<changeme>"
 }
 
 variable "password" {
   # If you don't fill this in, you will be prompted for it
-  default = "Tsi8675**"
+  default = "<changeme>"
 }
 
 variable "domain_name" {
   # If you don't fill this in, you will be prompted for it
-  default = "OTC00000000001000010501"
+  default = "<changeme>"
 }
 
 variable "tenant_name" {
-  #default = "eu-de_Nordea"
+  #default = "eu-de_<changeme>"
 }
 
 variable "endpoint" {
   default = "https://iam.eu-de.otc.t-systems.com:443/v3"
 }
 
-variable "kms_vol_key" {}
+variable "kms_vol_key" {
+}
 
-variable "bastion_count" {}
+variable "engine_count" {
+}
+
 #variable "existing_subnet_id" {}
+
 variable "availability_zone" {}
 variable "flavor_name" {}
 variable "image_id" {}
-#variable "zone_id" {}
+variable "zone_id" {}
+variable "admin_pass" {}
 variable "existing_private_net_id" {}
 
 #variable "user-data" {
