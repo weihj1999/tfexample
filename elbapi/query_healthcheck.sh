4d6f88b5425644f8ab488804fef3cd9e459279af
Wei HongJin <weihj1999@gmail.com>
2018-09-19 18:03:39 +0200

    example to update elb monitor

---
+++ elbapi/query_healthcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -v
+#post for token
+token=$(curl -0 -iv -X POST -v -H "content-type=application/json" https://iam.eu-de.otc.t-systems.com:443/v3/auth/tokens -d @auth.json 2>&1 | grep -m 1 -oP '(?<=X-Subject-Token:\s).*')
+
+#echo $token
+
+curl -iv -X GET -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: $token"  https://vpc.eu-de.otc.t-systems.com/v2.0/lbaas/healthmonitors/bc88dccd-687b-445a-8450-5879f5daa900
+
+#set th
+
+
---
+++ elbapi/query_listener.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "illegal number of parameters"
+    echo "Usage: ./$0 <listener-id>"
+    exit 1
+fi
+
+#post for token
+token=$(curl -0 -iv -X POST -v -H "content-type=application/json" https://iam.eu-de.otc.t-systems.com:443/v3/auth/tokens -d @auth.json 2>&1 | grep -m 1 -oP '(?<=X-Subject-Token:\s).*')
+
+echo $token
+
+curl -iv -X GET -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: $token" https://vpc.eu-de.otc.t-systems.com/v2.0/lbaas/listeners/$1
+
+#set th
+
+
