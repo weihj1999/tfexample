07890fc272d2f92322748b5f45cad7d7bfbdc4bc
Wei HongJin <weihj1999@gmail.com>
2018-09-21 17:50:06 +0200

    to support udpate and check with parameter

--- elbapi/query_healthcheck.sh
+++ elbapi/query_healthcheck.sh
@@ -1,10 +1,16 @@
-#!/bin/bash -v
+#!/bin/bash 
+if [ "$#" -ne 1 ]; then
+    echo "illegal number of parameters"
+    echo "Usage: ./$0 <monitor-id>"
+    exit 1
+fi
+
 #post for token
 token=$(curl -0 -iv -X POST -v -H "content-type=application/json" https://iam.eu-de.otc.t-systems.com:443/v3/auth/tokens -d @auth.json 2>&1 | grep -m 1 -oP '(?<=X-Subject-Token:\s).*')
 
 #echo $token
 
-curl -iv -X GET -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: $token"  https://vpc.eu-de.otc.t-systems.com/v2.0/lbaas/healthmonitors/bc88dccd-687b-445a-8450-5879f5daa900
+curl -iv -X GET -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: $token"  https://vpc.eu-de.otc.t-systems.com/v2.0/lbaas/healthmonitors/$1
 
 #set th
 
